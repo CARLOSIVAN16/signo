954fa57105a5f42e2971c92c5341aa437d23fdbe
Carlos <carlosperezcruz16@gmail.com>
2017-02-27 00:29:27 +0000

    Added username column, user profile pages, and username sign-up form.

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
   
   # Define Routes for Pages
   get '/home' => 'pages#home' #override default routes.
-  get '/profile' => 'pages#profile'
   get '/user/:id' => 'pages#profile'
   get '/explore' => 'pages#explore'
 
